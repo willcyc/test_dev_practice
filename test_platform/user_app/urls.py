a6659e36e5933bb84a2af7cb9bf7141979458fb6
leaning002 <leaning002@126.com>
2018-09-30 12:02:08 +0800

    完善登录功能

--- test_platform/user_app/urls.py
+++ test_platform/user_app/urls.py
@@ -6,4 +6,7 @@ app_name = 'user_app'
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^login_action/$',views.login_action,name='login_action'),
+    url(r'^project_manage/$',views.project_manage,name='project_manage'),
+    url(r'^logout/$',views.logout,name='logout'),
+    url(r'accounts/login/$',views.index,name='index'),
 ]
--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -17,12 +18,25 @@ def login_action(request):
 
         else:
             user = auth.authenticate(username=username,password=password)
-            print(user)
-            print(type(user))
             if user is not None:
                 auth.login(request,user)
-                return render(request,'user_app/project_manage.html')
+                #response = HttpResponseRedirect('/project_manage/')
+                #response.set_cookie('user',username,3600)
+                #return response
+                request.session['user'] = username
+                return HttpResponseRedirect('/project_manage/')
             else:
                 return render(request,'user_app/index.html',{'error': '用户名或密码错误！'})
+    else:
+        return render(request,'user_app/index.html')
 
+@login_required
+def project_manage(request):
+    #username = request.COOKIES.get('user','')
+    username = request.session.get('user','')
+    return render(request,'user_app/project_manage.html',{'user':username})
 
+def logout(request):
+    auth.logout(request)
+    response = HttpResponseRedirect('/')
+    return response
