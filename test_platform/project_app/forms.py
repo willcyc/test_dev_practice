532a5d0be26fa4e81a183565dbc4be24ec89ad53
willcyc <13839205941@163.com>
2018-10-20 17:47:16 +0800

    模块的创建、编辑、删除

---
+++ test_platform/project_app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from project_app.models import Project,Module
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['name','describe','status']
+
+class ModuleForm(forms.ModelForm):
+    class Meta:
+        model = Module
+        #fields = ['name','describe','status']
+        exclude = ['create_time']
--- test_platform/project_app/urls.py
+++ test_platform/project_app/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from project_app import views
+from project_app.views import project_views,module_views
 
 app_name = 'project_app'
 urlpatterns = [
-    path('project_manage/',views.project_manage,name="project_manage"),
-    path('add_project/',views.add_project,name="add_project"),
+    #项目管理
+    path('project_manage/',project_views.project_manage,name="project_manage"),
+    path('add_project/',project_views.add_project,name="add_project"),
+    path('edit_project/<int:pid>/',project_views.edit_project,name="edit_project"),
+    path('delete_project/<int:pid>/',project_views.delete_project,name="delete_project"),
+
+    #模块管理
+    path('module_manage/',module_views.module_manage,name="module_manage"),
+    path('add_module/',module_views.add_module,name="add_module"),
+    path('edit_module/<int:mid>/',module_views.edit_module,name="edit_module"),
+    path('delete_module/<int:mid>/',module_views.delete_module,name="delete_module"),
 ]
